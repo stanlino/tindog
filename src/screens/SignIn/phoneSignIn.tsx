868106b6cef85506508e25fddef5aa6ecceb6dee
Stanley Filipe <stanley92filipe@gmail.com>
2022-03-16 00:33:03 -0300

    feat: create the phoneSignIn screen

--- src/screens/SignIn/phoneSignIn.tsx
+++ src/screens/SignIn/phoneSignIn.tsx
@@ -1,12 +1,103 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
+
+import { Modal } from 'react-native'
 
 import { Container } from '@components/Container'
+import { BackButton } from '@components/BackButton'
+import { Button } from '@components/Button'
+import { Input } from '@components/Input'
 
 import { PhoneSignInScreenProps } from 'src/types/routes'
 
-export function PhoneSignIn({  } : PhoneSignInScreenProps){
+import { 
+  Title,
+  Form,
+  Label,
+  TextInputWrapper,
+  DDI
+} from './styles'
+
+export function PhoneSignIn({ navigation: { goBack } } : PhoneSignInScreenProps){
+
+  const [phoneNumberWithRegex, setPhoneNumberWithRegex] = useState('')
+  const [code, setCode] = useState('')
+  const [modalVisible, toggleModalVisible] = useReducer(isVisible => !isVisible, false)
+
+  const phoneNumber = phoneNumberWithRegex.replace(/[^\d]/g, "")
+
+  function handleSetPhoneNumber(text: string) {
+    if (phoneNumber.length >= 2 && phoneNumber.length < 7) {
+      setPhoneNumberWithRegex(text.replace(/^(\d{2})(\d)/, "($1) $2"))
+    } else if (phoneNumber.length === 7) {
+      setPhoneNumberWithRegex(text.replace(/^(.{10})(.)/, "$1-$2"))
+    } else {
+      setPhoneNumberWithRegex(text)
+    }
+  }
+
+  function handleSendPhoneNumber() {
+    toggleModalVisible()
+  }
+
   return (
     <Container>
+      <BackButton action={goBack} />
+
+      <Title>tindog</Title>
+        
+      <Form>
+
+        <Label>Digite seu número de telefone</Label>
+
+        <TextInputWrapper>
+          <DDI>+55</DDI>
+          <Input 
+            keyboardType='number-pad'
+            placeholder='63 99999 9999'
+            value={phoneNumberWithRegex}
+            onChangeText={handleSetPhoneNumber}
+            maxLength={15}
+          />
+        </TextInputWrapper>
+
+        <Button 
+          title="Enviar código para este número"
+          disabled={phoneNumber.length < 11}
+          onPress={handleSendPhoneNumber}
+        />
+
+      </Form>
+
+      <Modal statusBarTranslucent animationType='slide' visible={modalVisible} onRequestClose={toggleModalVisible}>
+        <Container>
+          <BackButton action={toggleModalVisible} close />
+
+          <Title>tindog</Title>
+
+          <Form>
+
+            <Label>{`Escreva o código enviado para \n${phoneNumberWithRegex}`}</Label>
+
+            <TextInputWrapper>
+              <Input 
+                keyboardType='number-pad'
+                placeholder='xxxxxx'
+                value={code}
+                onChangeText={setCode}
+                maxLength={5}
+              />
+            </TextInputWrapper>
+
+            <Button 
+              title="Enviar código de confirmação"
+              disabled={phoneNumber.length < 11}
+              onPress={handleSendPhoneNumber}
+            />
+
+          </Form>
+
+        </Container>
+      </Modal>
 
     </Container>
   )
--- src/screens/SignIn/styles.tsx
+++ src/screens/SignIn/styles.tsx
@@ -8,6 +8,7 @@ export const Header = styled.View`
 ` 
 
 export const Title = styled.Text`
+  text-align: center;
   ${({ theme }) => css`
     color: ${theme.COLORS.TITLE};
     font-size: ${RFValue(50)}px;
@@ -42,4 +43,34 @@ export const SubTitleText = styled.Text`
   `}
   margin-left: 5px;
   margin-right: 5px;
+`
+
+export const Form = styled.View`
+  flex: 1;
+  padding-top: ${RFValue(50)}px;
+`
+
+export const Label = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.TITLE};
+    font-size: ${RFValue(18)}px;
+    font-family: ${theme.FONTS.LIGHT};
+    margin-bottom: 10px;
+  `}
+`
+
+export const TextInputWrapper = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 10px;
+`
+
+export const DDI = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.TITLE};
+    font-size: ${RFValue(20)}px;
+    font-family: ${theme.FONTS.BOLD};
+    margin-right: 10px;
+  `}
 `
