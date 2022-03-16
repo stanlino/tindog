ed16ba60f8a56ab99dbb348198aee8ec7089f7ea
Stanley Filipe <stanley92filipe@gmail.com>
2022-03-16 00:31:38 -0300

    feat: create the Input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,23 @@
+import { RFValue } from 'react-native-responsive-fontsize'
+import styled, { css } from 'styled-components/native'
+
+export const Container = styled.TextInput`
+  flex: 1;
+  height: 55px;
+  
+  border-radius: 8px;
+
+  font-size: ${RFValue(20)}px;
+  line-height: ${RFValue(20)}px;
+
+  padding-left: ${RFValue(20)}px;
+  padding-right: ${RFValue(20)}px;
+
+  border: 1px solid #0003;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.REGULAR};
+    color: ${theme.COLORS.TEXT};
+  `};
+`
+
