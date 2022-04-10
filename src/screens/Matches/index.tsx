607a99580b992b5ae68b30b188331b9d24ab1b09
Stanley Filipe <stanley92filipe@gmail.com>
2022-04-09 23:03:41 -0300

    feat: update matches styles

--- src/screens/Matches/index.tsx
+++ src/screens/Matches/index.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import firestore from '@react-native-firebase/firestore'
 import { FlatList } from 'react-native'
+import AnimatedLottieView from 'lottie-react-native'
+import { RFPercentage } from 'react-native-responsive-fontsize'
 
 import { usePet } from '../../hooks/pet'
 import { Container } from '@components/Container'
-
-import { SvgStack } from './components/svgStack'
 
 import { 
   Title,
@@ -14,7 +14,10 @@ import {
   Side,
   Name,
   Touchable,
-  Text
+  Text,
+  TopDetail,
+  Content,
+  Output
 } from './styles'
 
 interface Match {
@@ -62,34 +65,51 @@ export function Matches(){
 
   return (
     <Container>
-      <Title>match's</Title>
-      <SvgStack />
-      <FlatList 
-        contentContainerStyle={{
-          paddingTop: 20,
-          paddingBottom: 80
-        }}
-        refreshing={isLoading}
-        onRefresh={getMatchs}
-        data={matchs}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => {
-
-          const imTheInterested = item.interestedID === currentPet.id
-
-          return (
-            <MatchView style={{ elevation: 5 }}>
-              <Avatar source={{ uri: imTheInterested ? item.interestingPhoto : item.interestedPhoto }} />
-              <Side>
-                <Name>{imTheInterested ? item.interestingName : item.interestedName}</Name>
-                <Touchable>
-                  <Text>Visualizar perfil</Text>
-                </Touchable>
-              </Side>
-            </MatchView>
-          )
-        }}
-      />
+      <TopDetail />
+      <Title>combinations</Title>
+      {matchs.length ? 
+        <FlatList 
+          contentContainerStyle={{
+            paddingTop: 20,
+            paddingBottom: 80
+          }}
+          refreshing={isLoading}
+          onRefresh={getMatchs}
+          data={matchs}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => {
+            const imTheInterested = item.interestedID === currentPet.id
+            return (
+              <MatchView style={{ elevation: 5 }}>
+                <Avatar source={{ uri: imTheInterested ? item.interestingPhoto : item.interestedPhoto }} />
+                <Side>
+                  <Name>{imTheInterested ? item.interestingName : item.interestedName}</Name>
+                  <Touchable>
+                    <Text>Visualizar perfil</Text>
+                  </Touchable>
+                </Side>
+              </MatchView>
+            )
+          }}
+        />
+      : (
+        <Content>
+          <AnimatedLottieView
+            source={require('@assets/lottie/dog-sleeping.json')}
+            style={{
+              width: RFPercentage(40),
+              
+            }}
+            autoPlay
+            loop
+            speed={0.5}
+          />
+          <Output>
+            Por enquanto não temos combinações :(
+          </Output>
+        </Content>
+        )
+      }
     </Container>
   )
 }
--- src/screens/Matches/styles.ts
+++ src/screens/Matches/styles.ts
@@ -1,14 +1,29 @@
 import styled, { css } from "styled-components/native"
 import { RFValue } from "react-native-responsive-fontsize"
+import { Dimensions } from "react-native"
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get('window')
 
 export const Title = styled.Text`
   text-align: left;
   ${({ theme }) => css`
-    color: ${theme.COLORS.PRIMARY};
+    color: ${theme.COLORS.SHAPE};
     font-size: ${RFValue(30)}px;
     font-family: ${theme.FONTS.BOLD};
   `}
 `
+
+export const TopDetail = styled.View`
+  width: ${screenWidth}px;
+  height: ${screenHeight * .35}px;
+
+  top: 0px;
+  left: 0px;
+
+  position: absolute;
+  background-color: ${({ theme }) => theme.COLORS.PRIMARY};
+`
+
 export const MatchView = styled.View`
   background-color: ${({ theme }) => theme.COLORS.SHAPE};
 
@@ -52,4 +67,20 @@ export const Text = styled.Text`
     font-size: ${RFValue(15)}px;
     font-family: ${theme.FONTS.BOLD};
   `}
+`
+
+export const Content = styled.View`
+  flex: 1;
+  padding-top: 50px;
+  align-items: center;
+`
+
+export const Output = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.TITLE};
+    font-family: ${theme.FONTS.BOLD};
+  `}
+  font-size: ${RFValue(20)}px;
+  text-align: center;
+  margin-top: 30px;
 `
