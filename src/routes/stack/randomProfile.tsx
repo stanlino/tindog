db81820e6d0030913cef5b074d62b41a40de0661
Stanley Filipe <stanley92filipe@gmail.com>
2022-04-09 23:00:54 -0300

    feat: create stack navigation routes

---
+++ src/routes/stack/randomProfile.tsx
@@ -0,0 +1,31 @@
+import { createSharedElementStackNavigator } from 'react-navigation-shared-element'
+
+import { Home } from '@screens/Home'
+import { Matches } from '@screens/Matches'
+import { RandomProfile } from '@screens/RandomProfile'
+
+import { RandomRrofileRoutesParams } from '../../types/routes'
+
+const { Navigator, Screen } = createSharedElementStackNavigator<RandomRrofileRoutesParams>()
+
+export function RamdomProfileRoutes(props : any) {
+
+  const route = props.route.params.route
+
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      { route === 'home' ? 
+        <Screen component={Home} name='index'/> :
+        <Screen component={Matches} name='index' /> 
+      }
+      <Screen 
+        component={RandomProfile} 
+        name='randomProfile' 
+        sharedElements={(route, otherRoute, showing) => {
+          const item = route.params
+          return [item.id]
+        }}
+      />
+    </Navigator>
+  )
+}
