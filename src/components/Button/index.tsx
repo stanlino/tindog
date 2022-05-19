63e82e44ae311fe8747e6285a4660cdd18bf6785
Stanley Filipe <stanley92filipe@gmail.com>
2022-05-19 17:56:55 -0300

    feat: uptade Button component style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,26 +1,58 @@
 import React from 'react'
-import { TouchableOpacityProps } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { useAnimationState } from 'moti'
+import { RFPercentage } from 'react-native-responsive-fontsize';
 
 import {
+  Bottom,
   Container,
-  Title
+  Title,
+  Wrapper
 } from './styles'
 
-interface ButtonProps extends TouchableOpacityProps {
+interface ButtonProps extends TouchableOpacity {
   title: string
   loading?: boolean
 }
+
+const ButtonHeight = RFPercentage(8)
 
 export function Button({
   title,
   loading = false,
   ...rest
 } : ButtonProps){
+
+  const buttonAnimation = useAnimationState({
+    pressIn: {
+      transform: [{ translateY: ButtonHeight * 0.1 }]
+    },
+    pressOut: {
+      transform: [{ translateY: 0 }]
+    }
+  })
+
+  function animateButton(state: 'pressIn' | 'pressOut') {
+    buttonAnimation.transitionTo(state)
+  }
+
   return (
-    <Container disabled={loading} {...rest}>
-      <Title numberOfLines={1} adjustsFontSizeToFit>
-        {loading ? 'Carregando ...' : title}
-      </Title>
+    <Container 
+      disabled={loading}
+      onPressIn={() => animateButton('pressIn')} 
+      delayPressOut={1}
+      onPressOut={() => animateButton('pressOut')} 
+      {...rest}
+    >
+      <Wrapper 
+        state={buttonAnimation} 
+        disabled={loading}
+      >
+        <Title numberOfLines={1} adjustsFontSizeToFit>
+          {loading ? 'Carregando ...' : title}
+        </Title>
+      </Wrapper>
+      <Bottom disabled={loading} />
     </Container>
   )
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,12 +1,26 @@
 import styled, { css } from 'styled-components/native'
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
+import { TouchableOpacityProps } from 'react-native';
+import { MotiView } from 'moti';
 
-export const Container = styled.TouchableOpacity`
+const ButtonHeight = RFPercentage(8)
+
+export const Container = styled.TouchableOpacity.attrs({
+  activeOpacity: 1
+})`
   width: 100%;
-  height: ${RFPercentage(8)}px;
-  background-color: ${({ theme, disabled }) => disabled ? '#0003' : '#333b89'};
+  height: ${ButtonHeight}px;
+`
+
+export const Wrapper = styled(MotiView)<TouchableOpacityProps>`
+  height: ${ButtonHeight * 0.9}px;
+  width: 100%;
+  background-color: ${({ theme, disabled }) => disabled ? '#a3a3a3' : '#333b89'};
   
   border-radius: 8px;
+
+  position: absolute;
+  z-index: 2;
 
   justify-content: center;
   align-items: center;
@@ -16,12 +30,22 @@ export const Container = styled.TouchableOpacity`
   padding-right: 20px;
 `
 
+export const Bottom = styled.View<TouchableOpacityProps>`
+  height: ${ButtonHeight/2}px;
+  width: 100%;
+  align-self: center;
+  background-color: ${({ theme, disabled }) => disabled ? '#6e6e6e' : '#1a2269'};
+  position: absolute;
+  bottom: 0;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+`
+
 export const Title = styled.Text`
   ${({ theme }) => css`
     color: ${theme.COLORS.SHAPE};
     font-size: ${RFValue(18)}px;
     font-family: ${theme.FONTS.BOLD};
     text-align: center;
-    flex: 1
   `}
 `
